2a3a65e88fa90155b5d9664c7de9c569df053b6e
jacques onek <onekongajaque@gmail.com>
2025-06-28 09:07:34 +0200

    ajou du route de email js

---
+++ app/api/emailjs/route.ts
@@ -0,0 +1,29 @@
+
+import { NextRequest, NextResponse } from "next/server";
+import emailjs from "@emailjs/nodejs";
+import config from "@/lib/config";
+
+export async function POST(req: NextRequest) {
+  const { email, subject, message } = await req.json();
+
+  try {
+    const result = await emailjs.send(
+      config.env.emaijs.emailId!,
+      config.env.emaijs.emailTemplateId!,
+      {
+        user_email: email,
+        subject,
+        message,
+      },
+      {
+        publicKey: config.env.emaijs.emailPublicKey!,
+        privateKey: config.env.emaijs.emailPrivateKey!,
+      }
+    );
+
+    return NextResponse.json({ success: true, result });
+  } catch (error) {
+    console.error("Erreur EmailJS:", error);
+    return NextResponse.json({ success: false, error }, { status: 500 });
+  }
+}
