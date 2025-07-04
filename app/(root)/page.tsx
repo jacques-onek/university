c6e35f44cc11d7a557c354696a61b950d1f75070
jacques onek <onekongajaque@gmail.com>
2025-07-04 07:11:20 +0200

    je cree le requete db pour les passer au composant principal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,27 @@
+import { auth } from '@/auth'
 import BookList from '@/components/BookList'
 import BookOverview from '@/components/BookOverview'
 import { sampleBooks } from '@/constant'
 import { db } from '@/database/drizzle'
-import { users } from '@/database/schema'
+import { books, users } from '@/database/schema'
+import { Book } from '@/types'
+import { desc } from 'drizzle-orm'
 import React from 'react'
 
 const Home = async  () => {
 
+const userId = (await auth())?.user?.id
+const latestBooks = (await db.select().from(books).limit(10).orderBy(desc(books.createdAt))) as Book[]
 
   return (
      <>
        <BookOverview
-        {...sampleBooks[0]}
+        {...latestBooks[0]}
        />
 
        <BookList
          title="Latest Books"
-         Books={sampleBooks}
+         Books={latestBooks}
          containerClassName="mt-28"
        />
        
