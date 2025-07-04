a46859b3b7724c29201219d3688604055a772ae5
jacques onek <onekongajaque@gmail.com>
2025-07-04 07:08:41 +0200

    j appele le donnee depuis la db

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -6,28 +6,14 @@ import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import { Button } from './ui/button'
 
-const BookCard = ({id,title,genre,color,cover,isLoanedBook = false}:Book) => <li>
+const BookCard = ({id,title,genre,coverColor,coverUrl}:Book) => <li>
     <Link href={`/book/${id}`}>
-      <BookCover coverColor={color} coverImage={cover}/>
-      <div className={cn("mt-4",!isLoanedBook && "xs:max-w-40 max-w-28" )}>
+      <BookCover coverColor={coverColor} coverImage={coverUrl}/>
+      <div className={cn("mt-4", "xs:max-w-40 max-w-28" )}>
         <p className='book-title'>{title}</p>
          <p className='book-genre'> {genre} </p>
       </div>
-      {isLoanedBook && (
-        <div className='mt-3 w-full'>
-          <div className='book-loaned'>
 
-           <Image
-           src="/icons/calendar.svg"
-           alt='calendar'
-           width={18}
-           height={18}
-           className='object-contain'/>
-           <p className='text-light-100'>11 days left to return</p>
-          </div>
-           <Button className='book-btn'> Download receipt</Button>
-        </div>
-      )}
     </Link>
 </li>
 export default BookCard
